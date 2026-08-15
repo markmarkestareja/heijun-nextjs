95ce1da0010b252f4890011708abbbb91a57ea9f
Mark Estareja <mark.estareja.me@gmail.com>
2026-08-15 15:23:23 +0800

    Added ProductCategories

--- data/ProductCategories.ts
+++ data/ProductCategories.ts
@@ -1,1 +1,27 @@
-export const
+export const ProductCategories = [
+    {
+        image: "amenities.webp",
+        title: "Hotel Amenities",
+        description: "Delight your guests with premium hotel essentials such as toiletries, bath kits, and spa-inspired items designed to elevate their stay and reflect your brand’s hospitality."
+    },
+    {
+        image: "guest.webp",
+        title: "Guest Room Essentials",
+        description: "From sleek kettles to stylish trays and hangers, provide every detail your guests need for a comfortable and convenient in-room experience."
+    },
+    {
+        image: "amenities.webp",
+        title: "Housekeeping Products",
+        description: "Equip your team with durable, professional-grade cleaning supplies and tools to maintain spotless rooms and uphold the highest hygiene standards."
+    },
+    {
+        image: "amenities.webp",
+        title: "Pillow and Linens",
+        description: "Offer luxurious comfort with plush pillows, crisp bed linens, and soft bedding that ensure a restful and memorable night’s sleep for every guest."
+    },
+    {
+        image: "amenities.webp",
+        title: "Other Products",
+        description: "Create a welcoming first impression with elegant lobby accessories—from décor pieces to practical fixtures that enhance the look and functionality of your reception area."
+    },
+]
