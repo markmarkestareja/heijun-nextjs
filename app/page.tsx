2233f4887cf71b207ae6b07ee675ff87014f4f37
Mark Estareja <mark.estareja.me@gmail.com>
2026-08-19 16:26:11 +0800

    Started building About section

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Hero from "@/components/home/Hero";
 import BrandLogos from "@/components/BrandLogos";
 import Offer from "@/components/home/Offer";
 import ProductCategories from "@/components/home/ProductCategories";
+import About from "@/components/home/About";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <BrandLogos />
       <Offer />
       <ProductCategories />
+      <About />
     </div>
   );
 }
--- components/home/About.tsx
+++ components/home/About.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 export default function About() {
   return (
     <section>
-      
+      <div
+        className="h-96 w-full"
+      >
+        <Image 
+            src="/images/hero.webp"
+            alt="About Us"
+            fill
+        />
+      </div>
     </section>
-  )
+  );
 }
