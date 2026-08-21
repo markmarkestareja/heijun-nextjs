e1e9f82f4226ede5f288063ea47dd8e0c08a15f8
Mark Estareja <mark.estareja.me@gmail.com>
2026-08-21 12:39:07 +0800

    Fixed About section

--- components/home/About.tsx
+++ components/home/About.tsx
@@ -10,12 +10,14 @@ export default function About() {
         flex
         flex-col
         lg:flex-row
+        items-center
         gap-9
         lg:pr-0!
-        border
+        h-lvh
+        bg-(image:--gradient)
       "
     >
-      <div className="relative flex-1 text-center flex flex-col gap-2 items-center">
+      <div className="relative lg:flex-1 text-center flex flex-col gap-2 items-center">
         <h2>About Us</h2>
         <p>
           HEIJUN is a wholesaling company that provides high quality and
@@ -25,10 +27,10 @@ export default function About() {
           products, restaurant equipment for guests, housekeeping supplies and
           lobby products.
         </p>
-        <ButtonPrimary link="/about" label="Learn More" addClass="Block w-max mt-10"/>
+        <ButtonPrimary link="/about" label="Learn More" addClass="Block w-max mt-9"/>
       </div>
 
-      <div className="relative h-96 w-full lg:flex-1 rounded-xl lg:rounded-l-xl lg:rounded-r-none overflow-hidden">
+      <div className="relative h-full w-full lg:flex-1 rounded-xl lg:rounded-l-xl lg:rounded-r-none overflow-hidden pt-10 pb-10">
         <Image
           src="/images/home/hero.webp"
           alt="About Us"
