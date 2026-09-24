c2f2a1e1480b1f0e443ae7d3780ab312f3029342
Mark Estareja <mark.estareja.me@gmail.com>
2026-09-24 19:54:41 +0800

    Dynamic title and description of product category page

--- app/product/[category]/page.tsx
+++ app/product/[category]/page.tsx
@@ -3,10 +3,17 @@ import { notFound } from "next/navigation";
 import type { ProductCategory } from "@/types/product";
 import Footer from "@/components/Footer";
 
-const categories: ProductCategory[] = [
-  "hotel-amenities",
-  "linens-and-towels",
-];
+const categories = {
+  "hotel-amenities": {
+    title: "Hotel Amenities",
+    description: "Explore our range of hotel amenities designed to provide comfort and convenience for guests.",
+  },
+  "linens-and-towels":{
+    title: "Linens and Towels",
+    description:
+      "Discover quality linens and towels designed for comfort, durability, and everyday hospitality use.",
+  },
+} as const;
 
 type Props = {
   params: Promise<{
@@ -17,21 +24,23 @@ type Props = {
 export default async function ProductCategoryPage({ params }: Props) {
   const { category } = await params;
 
-  if (!categories.includes(category as ProductCategory)) {
+  
+
+  if (!(category in categories)) {
     notFound();
   }
 
   const productCategory = category as ProductCategory;
+  const categoryData = categories[productCategory];
 
-  if (!productCategory) {
-    notFound();
-  }
+
+
 
   return (
     <>
       <section className="pt-25! lg:pt-30! border flex flex-col gap-9 bg-yellow2 text-dark">
-        <h1>{category.replaceAll("-", " ")}</h1>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem officia quisquam quia atque qui nihil?</p>
+        <h1>{categoryData.title}</h1>
+        <p>{categoryData.description}</p>
 
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-2 md:gap-4">
           <ProductCard productCategory={productCategory} />
